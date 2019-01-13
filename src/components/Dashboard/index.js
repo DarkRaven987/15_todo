8a448fa7c6c9e2e4a4fd20440c98dadef1077e68
DarkRaven987 <darkraven987@gmail.com>
2019-01-13 18:16:20 +0200

    Can set status 'Done' from task

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,27 +1,72 @@
 import React from 'react';
 import './style.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { faTrashAlt, faCheck } from '@fortawesome/free-solid-svg-icons'
+
+import Tasks from '../../tasks/tasks';
 
 class Dashboard extends React.Component {
+
+    constructor(props){
+        super(props);
+
+        this.state={tasks: Tasks}
+    }
+
+
+    onTaskIsDoneHandler = (id) => {
+        console.log("Is done!", id);
+        const {tasks} = this.state;
+
+        tasks.map((task) => {return (task.id === id) ? task.status=true:null});
+        this.setState({
+            tasks: tasks
+        })
+    };
+
+    onRemoveTaskHandler = (id) => {
+        console.log("Deleted! ",id);
+        const {tasks} = this.state;
+
+
+
+        this.setState({
+            tasks: tasks
+        })
+
+    };
+
     render(){
+
+        const {tasks} = this.state;
+
+        const html = tasks.map((el) => {
+
+            const isTaskDoneClass = el.status?  "TaskIsDone":"" ;
+
+            return(
+                <div className={`TaskElement col-12 col-md-8 col-lg-7 d-flex py-2 ${isTaskDoneClass}`} key={el.id}>
+                    <div className="TaskTitle col-2">
+                        {el.title}
+                    </div>
+                    <div className="TaskDesc d-flex col-7">
+                        {el.desc}
+                    </div>
+                    <div className="TaskControls col-3 d-flex justify-content-start align-items-start">
+                        {el.status? null : <button className="btn btn-outline-success" onClick={ () => {this.onTaskIsDoneHandler(el.id)}}>
+                            <FontAwesomeIcon icon={faCheck}/>
+                        </button>}
+                        <button className="btn btn-outline-danger" onClick={() => {this.onRemoveTaskHandler(el.id)}}><FontAwesomeIcon icon={faTrashAlt}/></button>
+                    </div>
+                </div>
+            );
+        });
+
         return(
             <div className="row my-5">
                 <div className="container">
                     <div className="row flex-column align-items-center">
-                        <div className="TaskElement col-12 col-md-8 col-lg-7 d-flex">
-                            <div className="TaskTitle col-2">
-                                Task Title #1
-                            </div>
-                            <div className="TaskDesc col-8">
-                                Task Description #1
-                            </div>
-                            <div className="TaskControls col-2">
-                                <button>Done</button>
-                                <button>Remove</button>
-                                <FontAwesomeIcon icon={faCoffee}/>
-                            </div>
-                        </div>
+                        {html}
                     </div>
                 </div>
             </div>
---
+++ src/tasks/tasks.js
@@ -0,0 +1,5 @@
+export default [
+    {id: 0, title: "Wake up", desc: "Get up frmo bed and go work.", status: true},
+    {id: 1, title: "Make coffee", desc: "Switch on a coffe machine and wait for a few minutes", status: false},
+    {id: 2, title: "Make breakfast", desc: "Open fridge, lok for food.", status: false}
+]
